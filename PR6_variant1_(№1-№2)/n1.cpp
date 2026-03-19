0dc53cf251232017423035fc683e1ea407c24d9f
UksovIlya <polala200@mail.ru>
2026-03-19 15:25:37 +0300

    Add files via upload

---
+++ PR6_variant1_(№1-№2)/n1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+class Message
+{
+private:
+	string text;
+
+public:
+	///Создаем сообщение
+	Message() {
+		cout << "Введите текст вашего сообщения: " << endl;
+		getline(cin, text);
+		cout << endl << "Создано сообщение: " << text << endl;
+	}
+	///Удаляем сообщение
+	~Message() {
+		cout << "Удалено сообщение: " << text << endl;
+	}
+
+	void print() {
+		cout << text << endl;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	//Создаем стандартное сообщение и через динамическое выделение памяти
+	Message first = Message();
+	Message* second = new Message();
+
+	first.print();
+	second->print();
+	delete(second);
+
+	return 0;
+}
+
+
---
+++ PR6_variant1_(№1-№2)/n2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+class Counter {
+private:
+    int id;
+    static int totalCounters;
+
+public:
+    ///Создаем счетчик
+    Counter() {
+        totalCounters++;
+        id = totalCounters;
+        cout << "Счётчик #" << id << " создан" << endl;
+    }
+    ///Удаляем счетчик
+    ~Counter() {
+        totalCounters--;
+        cout << "Счётчик #" << id << " уничтожен" << endl;
+    }
+
+    static int getTotalCounters() {
+        return totalCounters;
+    }
+};
+
+///Создаем глобальную переменную
+int Counter::totalCounters = 0;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    int N;
+    cout << "Сколько счётчиков создать? ";
+    cin >> N;
+
+    if (N <= 0) {
+        cout << "Количество счётчиков должно быть положительным числом!" << endl;
+        return 1;
+    }
+
+    //создаем динамический массив с счетчиками
+    Counter* counters = new Counter[N];
+    cout << "Текущее количество счётчиков: " << Counter::getTotalCounters() << endl;
+    delete[] counters;
+    cout << "После удаления массива количество счётчиков: " << Counter::getTotalCounters() << endl;
+
+    return 0;
+}
